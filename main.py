f224c558b057e9234a39ab651a0da6f113b76caa
Max Tulskiy <maks.tulskiy.04@gmail.com>
2024-03-09 21:03:34 +0300

    added insertion, updating, removing and reading data from db

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
 from window import Ui_MainWindow
 import sys
 import psycopg2
@@ -10,26 +10,107 @@ class MyMainWindow(QMainWindow):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        
+        self.ui.tableWidget.setColumnCount(4)
+
+        self.ui.tableWidget.setHorizontalHeaderLabels(["Код туриста", "Фамилия", "Имя", "Отчество"])
+
+        self.showDataFromDB()
+        
+        self.ui.pushButton_4.clicked.connect(self.addRow)
+        self.ui.pushButton_2.clicked.connect(self.editDataInDB)
+        self.ui.tableWidget.itemChanged.connect(self.editDataInDB)
+        self.ui.pushButton.clicked.connect(self.add_data_to_database)
+        self.ui.pushButton_3.clicked.connect(self.removeDataFromDb)
+
+    def showDataFromDB(self):
+        connection = psycopg2.connect(host=host, user=user, password=password, database=db_name)
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM tourist ORDER BY id")
+            rows = cursor.fetchall()
+
+            for row_number, row_data in enumerate(rows):
+                self.ui.tableWidget.insertRow(row_number)
+                for column_number, data in enumerate(row_data):
+                    self.ui.tableWidget.setItem(row_number, column_number, QTableWidgetItem(str(data)))
+        connection.close()
+
+    def addRow(self):
+        row_position = self.ui.tableWidget.rowCount()
+        self.ui.tableWidget.insertRow(row_position)
+
+    #Добавить
+    def add_data_to_database(self):
+        
+        num_rows = self.ui.tableWidget.rowCount()
+        connection = psycopg2.connect(host=host, user=user, password=password, database=db_name)
+        
+        for row in range(num_rows):
+            id_item = self.ui.tableWidget.item(row, 0)
+            name_item = self.ui.tableWidget.item(row, 1)
+            surname_item = self.ui.tableWidget.item(row, 2)
+            patronymic_item = self.ui.tableWidget.item(row, 3)
+
+            if not all([id_item, name_item, surname_item, patronymic_item]):
+                continue
+
+            id_value = id_item.text()
+            first_name_value = name_item.text()
+            second_name_value = surname_item.text()
+            patronymic_value = patronymic_item.text()
+
+            cursor = connection.cursor()
+            cursor.execute("SELECT id FROM tourist WHERE id = %s", (id_value,))
+            existing_id = cursor.fetchone()
+
+            if existing_id:
+                continue
+
+            cursor.execute("INSERT INTO tourist (id, first_name, second_name, patronymic) VALUES (%s, %s, %s, %s)", (id_value, first_name_value, second_name_value, patronymic_value))
+            connection.commit()
+
+            cursor.close()
+            connection.close()
+
+    #Изменить
+    def editDataInDB(self, item):
+        mappa = {'Код туриста': 'id', 'Фамилия': 'second_name', 'Имя': 'first_name', 'Отчество': 'patronymic'}
+        row = item.row()
+        column = item.column()
+        new_value = item.text()
+
+        id_item = self.ui.tableWidget.item(row, 0)
+        id_value = id_item.text()
+
+        connection = psycopg2.connect(host=host, user=user, password=password, database=db_name)
+        cursor = connection.cursor()
+        cursor.execute("UPDATE tourist SET {} = %s WHERE id = %s".format(mappa[self.ui.tableWidget.horizontalHeaderItem(column).text()]), (new_value, id_value))
+        connection.commit()
+        cursor.close()
+        connection.close()
+
+    #Удалить
+    def removeDataFromDb(self):
+        current_row = self.ui.tableWidget.currentRow()
+        connection = psycopg2.connect(host=host, user=user, password=password, database=db_name)
+
+        if current_row != -1:  
+            id_item = self.ui.tableWidget.item(current_row, 0)
+            id_value = id_item.text()
+
+            self.ui.tableWidget.removeRow(current_row)
+
+            cursor = connection.cursor()
+            cursor.execute("DELETE FROM tourist WHERE id = %s", (id_value,))
+            connection.commit()
+            cursor.close()
 
 
 def main():
-    #app = QApplication(sys.argv)
-    #mainWindow = MyMainWindow()
-    #mainWindow.show()
-    #sys.exit(app.exec())
-    try:
-        connection = psycopg2.connect(host=host, user=user, password=password, database=db_name)
-
-        with connection.cursor() as cursor:
-            cursor.execute("запрос тут")
-
-
-    except Exception as ex:
-        print("Не удалось подключиться к бд", ex)
-    finally:
-        if connection:
-            connection.close()
-            print("Коннект закрыт")
+    app = QApplication(sys.argv)
+    mainWindow = MyMainWindow()
+    mainWindow.show()
+    sys.exit(app.exec())
 
 
 if __name__ == "__main__":
--- window.py
+++ window.py
@@ -15,9 +15,9 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QComboBox, QHeaderView, QMainWindow,
-    QMenuBar, QPushButton, QSizePolicy, QStatusBar,
-    QTableView, QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QHeaderView, QMainWindow, QMenuBar,
+    QPushButton, QSizePolicy, QStatusBar, QTableWidget,
+    QTableWidgetItem, QVBoxLayout, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -26,33 +26,36 @@ class Ui_MainWindow(object):
         MainWindow.resize(800, 600)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.comboBox = QComboBox(self.centralwidget)
-        self.comboBox.setObjectName(u"comboBox")
-        self.comboBox.setGeometry(QRect(670, 50, 101, 31))
-        self.tableView = QTableView(self.centralwidget)
-        self.tableView.setObjectName(u"tableView")
-        self.tableView.setGeometry(QRect(30, 20, 601, 501))
-        self.widget = QWidget(self.centralwidget)
-        self.widget.setObjectName(u"widget")
-        self.widget.setGeometry(QRect(660, 170, 121, 181))
-        self.verticalLayout = QVBoxLayout(self.widget)
+        self.layoutWidget = QWidget(self.centralwidget)
+        self.layoutWidget.setObjectName(u"layoutWidget")
+        self.layoutWidget.setGeometry(QRect(660, 170, 121, 181))
+        self.verticalLayout = QVBoxLayout(self.layoutWidget)
         self.verticalLayout.setObjectName(u"verticalLayout")
         self.verticalLayout.setContentsMargins(0, 0, 0, 0)
-        self.pushButton = QPushButton(self.widget)
+        self.pushButton = QPushButton(self.layoutWidget)
         self.pushButton.setObjectName(u"pushButton")
 
         self.verticalLayout.addWidget(self.pushButton)
 
-        self.pushButton_3 = QPushButton(self.widget)
+        self.pushButton_3 = QPushButton(self.layoutWidget)
         self.pushButton_3.setObjectName(u"pushButton_3")
 
         self.verticalLayout.addWidget(self.pushButton_3)
 
-        self.pushButton_2 = QPushButton(self.widget)
+        self.pushButton_2 = QPushButton(self.layoutWidget)
         self.pushButton_2.setObjectName(u"pushButton_2")
 
         self.verticalLayout.addWidget(self.pushButton_2)
 
+        self.tableWidget = QTableWidget(self.centralwidget)
+        self.tableWidget.setObjectName(u"tableWidget")
+        self.tableWidget.setGeometry(QRect(30, 30, 601, 351))
+        self.tableWidget_2 = QTableWidget(self.centralwidget)
+        self.tableWidget_2.setObjectName(u"tableWidget_2")
+        self.tableWidget_2.setGeometry(QRect(30, 410, 601, 121))
+        self.pushButton_4 = QPushButton(self.centralwidget)
+        self.pushButton_4.setObjectName(u"pushButton_4")
+        self.pushButton_4.setGeometry(QRect(640, 30, 41, 28))
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
@@ -69,8 +72,9 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
-        self.pushButton_3.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"\u0414\u043e\u0431\u0430\u0432\u0438\u0442\u044c", None))
+        self.pushButton_3.setText(QCoreApplication.translate("MainWindow", u"\u0423\u0434\u0430\u043b\u0438\u0442\u044c", None))
         self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.pushButton_4.setText(QCoreApplication.translate("MainWindow", u"+", None))
     # retranslateUi
 
